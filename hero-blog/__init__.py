e12f76f1eb71f96869f010749f6f0f9b9abfc13c
SergioAnd95 <developinc@yadnex.ru>
2017-07-16 14:44:18 +0300

    rename directory

--- hero-blog/__init__.py
+++ hero-blog/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('app.settings')
+app.config.from_object('hero-blog.settings')
 
 db = SQLAlchemy(app)
