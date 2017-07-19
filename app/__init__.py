23ca02bab55bffd661f68076c3030748c011a9c5
SergioAnd95 <developinc@yadnex.ru>
2017-07-19 16:42:34 +0300

    Create model User for this needed: - Update local_settings.py.txt - Fix basedir in settings - Add Flask-Migrate for make migrations autonatic - Create folder migrations(use command 'python main.py db init') - Udpate main.py for work with console

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask_wtf.csrf import CSRFProtect
 
 
@@ -10,9 +12,15 @@ app.config.from_object('app.settings')
 
 db = SQLAlchemy(app)
 CSRFProtect(app)
+mograte = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 
 from .views import core_pages
 from users.views import user_pages
+
+from users.models import *
 
 app.register_blueprint(core_pages)
 app.register_blueprint(user_pages)
--- users/models.py
+++ users/models.py
@@ -1,1 +1,21 @@
-# todo: create model User
+from app import db
+from sqlalchemy_utils import ChoiceType, PasswordType
+
+
+class User(db.Model):
+    USER_ROLE = (
+        ('regular_user', 1),
+        ('admin', 2)
+    )
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(30), unique=True)
+    role = db.Column(ChoiceType(USER_ROLE), default=1)
+    password = db.Column(PasswordType(
+        schemes=[
+            'pbkdf2_sha512',
+            'md5_crypt'
+        ],
+        deprecated=['md5_crypt']
+    ))
